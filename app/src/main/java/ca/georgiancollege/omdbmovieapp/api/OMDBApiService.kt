e4d4cac7510d9f6e67b2734a88a94181f67c48bc
g0h1gh3r <youhengzeng8@gmail.com>
2025-07-24 19:38:45 -0400

    create API Service Class

---
+++ app/src/main/java/ca/georgiancollege/omdbmovieapp/api/OMDBApiService.kt
@@ -0,0 +1,51 @@
+package ca.georgiancollege.omdbmovieapp.api
+
+import org.json.JSONObject
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.URL
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import com.google.gson.Gson
+import ca.georgiancollege.omdbmovieapp.model.Movie
+import ca.georgiancollege.omdbmovieapp.model.MovieSearch
+import ca.georgiancollege.omdbmovieapp.model.MovieDetails
+
+class OMDBApiService {
+    private val apiKey = "f9b9618f"
+    private val baseUrl = "http://www.omdbapi.com/"
+    private val gson = Gson()
+
+    suspend fun searchMovies(query: String): MovieSearch? {
+        return withContext(Dispatchers.IO) {
+            try {
+                val url = URL("$baseUrl?apikey=$apiKey&s=$query")
+                val connection = url.openConnection() as HttpURLConnection
+                connection.requestMethod = "GET"
+
+                val response = connection.inputStream.bufferedReader().use { it.readText() }
+                gson.fromJson(response, MovieSearch::class.java)
+            } catch (e: Exception) {
+                e.printStackTrace()
+                null
+            }
+        }
+    }
+
+    suspend fun getMovieDetails(imdbId: String): MovieDetails? {
+        return withContext(Dispatchers.IO) {
+            try {
+                val url = URL("$baseUrl?apikey=$apiKey&i=$imdbId")
+                val connection = url.openConnection() as HttpURLConnection
+                connection.requestMethod = "GET"
+
+                val response = connection.inputStream.bufferedReader().use { it.readText() }
+                gson.fromJson(response, MovieDetails::class.java)
+            } catch (e: Exception) {
+                e.printStackTrace()
+                null
+            }
+        }
+    }
+}
