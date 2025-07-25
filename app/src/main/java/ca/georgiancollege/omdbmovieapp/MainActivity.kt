e13aa417b83c3c9d405daadde6d6617f45fdbd9b
g0h1gh3r <youhengzeng8@gmail.com>
2025-07-24 21:04:59 -0400

    delete uneccessary code

--- app/src/main/java/ca/georgiancollege/omdbmovieapp/MainActivity.kt
+++ app/src/main/java/ca/georgiancollege/omdbmovieapp/MainActivity.kt
@@ -1,17 +1,6 @@
 package ca.georgiancollege.omdbmovieapp
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import ca.georgiancollege.omdbmovieapp.ui.theme.OMDBMovieAppTheme
 import android.content.Intent
 import android.view.View
 import androidx.activity.viewModels
